324d5b6cd10aba2f3355d09a3df3342479531a7f
sayuj1 <sayuj.sehgal@gmail.com>
2024-01-28 08:41:38 +0530

    added welcome message for default route

--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,12 @@ const authRoute = require("./auth");
 const restaurantRoute = require('./restaurant')
 
 const configRoutes = app => {
+
   app.use("/auth", authRoute);
-  app.use("/restaurants", restaurantRoute)
+  app.use("/restaurants", restaurantRoute);
+  app.use('/', (req, res) => {
+    res.send('index.html');
+  });
 };
 
 module.exports = { configRoutes };
