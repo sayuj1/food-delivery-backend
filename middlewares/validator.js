dcb423865a82e9c9e5a49122b1549ba0e633aa3e
sayuj1 <sayuj.sehgal@gmail.com>
2024-01-28 17:41:15 +0530

    correct error message

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -10,7 +10,7 @@ exports.validateAuthInputFields = (req, res, next) => {
   if (password.trim() === "") {
     allErrors.password = "Password is Required";
   } else if (password.trim().length < 8) {
-    allErrors.password = "Password shoulbe have mininum 8 Characters";
+    allErrors.password = "Password should have mininum 8 Characters";
   }
 
   if (Object.values(allErrors).some((error) => error)) {
